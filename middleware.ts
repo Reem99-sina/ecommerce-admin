2172fd26a4d7b92df940c58022e6c6cf797be7e7
Reem99-sina <reemsina2@gmail.com>
2024-03-29 21:55:49 +0200

    whic

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,25 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+
+export function middleware() {
+    // retrieve the current response
+    const res = NextResponse.next()
+
+    // add the CORS headers to the response
+    res.headers.append('Access-Control-Allow-Credentials', "true")
+    res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    res.headers.append(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+
+    return res
+}
+
 export default authMiddleware({
-  
+  apiRoutes:["/api/:path*","/api/stores"],
   publicRoutes: ["/api/:path*","/api/stores"],
   
   // publicRoutes: "/api/550a06cf-d280-4fdd-a959-bcb66dfe60c8/categories"
