b6b1516823c94472f73417aeb739f68982c68b49
Reem99-sina <reemsina2@gmail.com>
2024-03-29 23:10:15 +0200

    whic

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 
 export default authMiddleware({
   apiRoutes:["/api/:path*","/api/stores"],
-  publicRoutes: ["/api/:path*","/api/stores"]
+  ignoredRoutes: ["/api/:path*","/api/stores"]
   // publicRoutes: "/api/550a06cf-d280-4fdd-a959-bcb66dfe60c8/categories"
 });
  
